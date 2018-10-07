952eb1ec37dbda14a8e0b30868a7bfcd9c29e9b2
ArtiusAstro <28533161+ArtiusAstro@users.noreply.github.com>
2018-10-07 06:59:29 -0700

    ok

--- lab4/src/Q2.java
+++ lab4/src/Q2.java
@@ -4,7 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
- * Find shortest path between two graph vertices using BFS
+ * Find shortest path between two unweighted graph vertices using BFS
  *
  * @author Ayub Atif
  */
@@ -12,7 +12,7 @@ public class Q2 {
     public static void main(String[] args) throws FileNotFoundException {
         System.out.println("--------------------\nQ2\n--------------------");
 
-        GraphX<String> grX = new GraphX<>();
+        GraphX<String> grX = new UnDiGraph<>();
         String src; String dst;
         try(Scanner sc = new Scanner(new File("contiguous-usa.txt"))) {
             while(sc.hasNext()){
@@ -29,11 +29,42 @@ public class Q2 {
             e.printStackTrace();
         }
 
-        grX.printGraph();
+        //grX.printGraph();
         System.out.println("N: "+grX.getN());
         System.out.println("E: "+grX.getE()+"\n");
 
-        System.out.println(grX.BFShortsetNoWeight("CA","NY").UnDiPath());
+        //Symmetry
+        System.out.println(grX.BFShortsetNoWeight("OH","NY").UnDiPath());
+        System.out.println(grX.BFShortsetNoWeight("NY","OH").UnDiPath());
+        //Empty path
+        System.out.println(grX.BFShortsetNoWeight("CA","CA").UnDiPath());
+        //Long path
+        System.out.println(grX.BFShortsetNoWeight("CA","FL").UnDiPath());
+        //Disconnected src & dst
+        System.out.println(grX.BFShortsetNoWeight("CA","DXB").UnDiPath());
     }
+
+    /*
+    --------------------
+    Q2
+    --------------------
+    N: 49
+    E: 107
+
+    |OH->NY|
+    Shortest Distance: 2
+    Shortest Path: [OH]-[PA]-[NY]
+    |NY->OH|
+    Shortest Distance: 2
+    Shortest Path: [NY]-[PA]-[OH]
+    |CA->CA|
+    Shortest Distance: 0
+    Shortest Path: [CA]
+    |CA->FL|
+    Shortest Distance: 7
+    Shortest Path: [CA]-[AZ]-[NM]-[OK]-[AR]-[MS]-[AL]-[FL]
+    |CA->DXB|
+    Disconnected src & dst
+     */
 }
 
